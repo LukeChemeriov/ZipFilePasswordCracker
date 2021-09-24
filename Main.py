3319c61f75b7add192d2c10f81af833640eec3a6
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-09-24 18:16:52 +0100

    Update Main.py
    Setting up for future updates

--- Main.py
+++ Main.py
@@ -4,13 +4,13 @@ import zipfile
 
 count = 1
 
-print("Enter name of zip file:")
+print("Enter name of zip file (include zip extension):")
 name = input()
 with open('passwords.txt','rb') as text:
     for entry in text.readlines():
         password = entry.strip()
         try:
-            with zipfile.ZipFile('locked1.zip','r') as zf:
+            with zipfile.ZipFile(input,'r') as zf:
                 zf.extractall(pwd=password)
 
                 data = zf.namelist()[0]
@@ -19,6 +19,7 @@ with open('passwords.txt','rb') as text:
 
                 print('''******************************\n[+] Password found! ~ %s\n ~%s\n ~%s\n******************************''' 
                     % (password.decode('utf8'), data, data_size))
+                cracked = true
                 break
 
         except:
@@ -26,3 +27,10 @@ with open('passwords.txt','rb') as text:
             print('[%s] [-] Password failed! - %s' % (number,password.decode('utf8')))
             count += 1
             pass
+            cracked = false
+if (cracked == true):
+    exit(0)
+else:
+    print("Password could not be found. Do you wish to try again with a different dictionary?")
+    confirmyn = input().lower()
+       
