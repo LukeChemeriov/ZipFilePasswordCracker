4b67650c9f62741524e405c25f67fd98f4cf250e
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-09-24 19:08:39 +0100

    Update Main.py
    Added extra functionality allowing for multiple dictionaries

--- Main.py
+++ Main.py
@@ -6,31 +6,37 @@ count = 1
 
 print("Enter name of zip file (include zip extension):")
 name = input()
-with open('passwords.txt','rb') as text:
-    for entry in text.readlines():
-        password = entry.strip()
-        try:
-            with zipfile.ZipFile(input,'r') as zf:
-                zf.extractall(pwd=password)
+def testPassword(passwordlist, filename):
+    with open('passwords.txt','rb') as text:
+         for entry in text.readlines():
+            password = entry.strip()
+            try:
+                with zipfile.ZipFile(filename,'r') as zf:
+                    zf.extractall(pwd=password)
 
-                data = zf.namelist()[0]
+                    data = zf.namelist()[0]
 
-                data_size = zf.getinfo(data).file_size
+                    data_size = zf.getinfo(data).file_size
+ 
+                    print('''******************************\n[+] Password found! ~ %s\n ~%s\n ~%s\n******************************''' 
+                        % (password.decode('utf8'), data, data_size))
+                    cracked = true
+                    break
 
-                print('''******************************\n[+] Password found! ~ %s\n ~%s\n ~%s\n******************************''' 
-                    % (password.decode('utf8'), data, data_size))
-                cracked = true
-                break
-
-        except:
-            number = count
-            print('[%s] [-] Password failed! - %s' % (number,password.decode('utf8')))
-            count += 1
-            pass
-            cracked = false
+            except:
+                number = count
+                print('[%s] [-] Password failed! - %s' % (number,password.decode('utf8')))
+                count += 1
+                pass
+                cracked = false
+testPassword(passwords.txt)
 if (cracked == true):
     exit(0)
 else:
     print("Password could not be found. Do you wish to try again with a different dictionary?")
     confirmyn = input().lower()
+    if (confirmyn == "y"):
+        testPassword(passwords2.txt)
+    else:
+        exit/(-1)
        
